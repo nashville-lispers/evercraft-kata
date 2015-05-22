4e12074627449bc09eafe4c20fb079fc03e1f4ba
Nate West <natescott.west@gmail.com>
2015-05-21 20:05:22 -0500

    Validate alignment

--- niftyn8+clojure/src/evercraft/character.clj
+++ niftyn8+clojure/src/evercraft/character.clj
@@ -2,6 +2,14 @@
 
 (defrecord GameCharacter [name alignment ac hp])
 
+(def valid-alignments #{:good :evil :neutral})
+
+(defn- validate [char-map]
+  (or ((get char-map :alignment :invalid) valid-alignments)
+      (throw (Exception. "Not a valid alignment")))
+  char-map)
+
 (defn make-character [& [opts]]
-  (let [defaults {:ac 10 :hp 5}]
-    (map->GameCharacter (merge defaults opts))))
+  (let [defaults {:ac 10 :hp 5 :alignment :neutral}
+        char-map (validate (merge defaults opts))]
+    (map->GameCharacter char-map)))
