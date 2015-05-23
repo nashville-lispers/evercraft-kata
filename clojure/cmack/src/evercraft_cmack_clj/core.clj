5beb5a5a13ce3652524f8d1e7323ef8e35660773
Charlie McMackin <charliemac@gmail.com>
2015-05-23 12:03:45 -0500

    Commit progess so far
    * Upto initialization of character abilities

--- clojure/cmack/src/evercraft_cmack_clj/core.clj
+++ clojure/cmack/src/evercraft_cmack_clj/core.clj
@@ -1,1 +1,62 @@
 (ns evercraft-cmack-clj.core)
+
+(defrecord character
+    [name alignment hit-points armor-class abilities])
+
+(defrecord abilities
+    [strength dexterity constitution wisdom intelligence charisma])
+
+(defn ensure-alignment
+  [alignment]
+  (let [allowed-alignments #{:Good :Evil :Neutral}]
+    (when (contains? allowed-alignments alignment)
+      alignment)))
+
+(defn make-abilities
+  [ability-map]
+  (let [default-abilities {:strength 10
+                           :dexterity 10
+                           :constitution 10
+                           :wisdom 10
+                           :intelligence 10
+                           :charisma 10}]
+    (map->abilities (merge default-abilities ability-map))))
+
+(defn make-character
+  "Create a character record"
+  [& {:keys [name alignment hit-points armor-class abilities]
+      :or {armor-class 10 hit-points 5}}]
+  (let [alignment (ensure-alignment alignment)
+        abilities (make-abilities abilities)]
+   (character. name alignment hit-points armor-class abilities)))
+
+
+(defn damage
+  [target damage]
+  (assoc target :hit-points (- (:hit-points target) damage)))
+
+(defn critical-hit?
+  [roll]
+  (= roll 20))
+
+(defn hit?
+  [character roll]
+  (or (>= roll (:armor-class character))
+      (critical-hit? roll)))
+
+(defn dead?
+  [character]
+  (<= (:hit-points character) 0))
+
+(defn modifier
+  [ability-score]
+  (- (Math/floor (/ ability-score 2))
+     5))
+
+(defn attack
+  [attacker target roll]
+  ;; todo range check roll
+  (cond
+    (critical-hit? roll) (damage target 2)
+    (hit? target roll) (damage target 1)
+    :else target))
--- clojure/cmack/test/evercraft_cmack_clj/core_test.clj
+++ clojure/cmack/test/evercraft_cmack_clj/core_test.clj
@@ -1,7 +1,45 @@
 (ns evercraft-cmack-clj.core-test
-  (:require [clojure.test :refer :all]
-            [evercraft-cmack-clj.core :refer :all]))
+  (:use clojure.test
+        evercraft-cmack-clj.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest character-init
+  (let [me (make-character :name "charlie")]
+    (testing "Character creation"
+      ;; (is (instance? character me) "character creation")
+      (is (contains? me :name) "has a name")
+      (is (= (:name me) "charlie") "get character name")
+      (is (= (:armor-class me) 10) "default armor class")
+      (is (= (:hit-points me) 5) "default hit points"))
+    (testing "Character name set"
+      (let [me (assoc me :name "not charlie")]
+        (is (= (:name me) "not charlie") "set character name")))))
+
+(deftest character-alignment
+  (let [me (make-character :name "charlie" :alignment :Good)]
+    (testing "Alignment init"
+      (is (contains? me :alignment) "has alignment")
+      (is (contains? #{:Good :Evil :Neutral} (:alignment me))))
+    (testing "Alignment set"
+      (let [me (assoc me :alignment :Evil)]
+        (is (= (:alignment me) :Evil) "set alignment")))))
+
+(deftest character-abilities
+  (let [me (make-character :name "charlie")]
+    (testing "Default values"
+      (is (contains? me :abilities) "has abilities")
+      (is (every? #{10} (vals (:abilities me))) "default ability to 10"))))
+
+(deftest character-attack
+  (let [attacker (make-character :name "attack")
+        defender (make-character :name "defend")]
+    (testing "Critical Hit"
+      (is (= 3 (:hit-points (attack attacker defender 20))) "critical hit damage")
+      (let [defender (assoc defender :armor-class 100)]
+        (is (hit? defender 20) "critical hit on high AC defender")
+        (is (= 3 (:hit-points (attack attacker defender 20))) "crit damage on high AC defender")))
+    (testing "Basic Hits"
+      (is (= 4 (:hit-points (attack attacker defender 10))) "roll == AC hit")
+      (is (= 4 (:hit-points (attack attacker defender 11))) "roll > AC hit"))
+    (testing "Death"
+      (is (dead? (damage defender 100)) "dead on overwhelming damage")
+      (is (dead? (damage defender 5)) "dead on exact damage"))))
